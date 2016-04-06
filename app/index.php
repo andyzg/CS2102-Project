b1f0b19aad641aeff6cdfda965e0c73f4a81c418
Jack Song <jackthesong@gmail.com>
2016-04-06 17:54:51 +0800

    allow registering

--- app/index.php
+++ app/index.php
@@ -6,8 +6,12 @@
 
 <form action="login.php">
   <h1> Login </h1>
-  Username: <input type="text" name="username">
-  <input type="submit" value="Login">
+  Username: <input type="text" name="username"><br>
+  First name: <input type="text" name="firstname"><br>
+  Last name: <input type="text" name="lastname"><br>
+  Email: <input type="text" name="email"><br>
+  <input type="submit" name="submitBtn" value="Login">
+  <input type="submit" name="submitBtn" value="Register">
 <?php
 if (isset($_GET['error']) && $_GET['error'] == 1) {
   echo "<p>Error: Username does not exist.</p>";
--- app/login.php
+++ app/login.php
@@ -2,6 +2,20 @@
 require('database_connection.php');
 
 if (isset($_GET['username'])) {
+
+  //registering
+  if($_GET['submitBtn'] == "Register") {
+    $countquery = "SELECT COUNT(*) as total FROM carpool.carpool.users";
+    $countresult = pg_query($query) or die('Query failed: ' . pg_last_error());
+    $rowcount = pg_fetch_row($countresult);
+
+    $query = "INSERT INTO carpool.carpool.users VALUES ('".$_GET['username']."', '".$_GET['email']."', '".$_GET['firstname']."', '".$_GET['lastname']."', ".($rowcount[0] + 1).", false)";
+    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+    if ($pg_num_rows($result) == 0) {
+      header("Location: http://localhost:8080/CS2102-Project/app/index.php?error=1");
+    }
+  }
+
   $query = "SELECT username, is_admin FROM carpool.carpool.users u WHERE u.username = '".$_GET['username']."'";
   $result = pg_query($query) or die('Query failed: ' . pg_last_error());
   header("Location: http://localhost:8080/CS2102-Project/app/index.php");
@@ -11,6 +25,7 @@ if (isset($_GET['username'])) {
     $row = pg_fetch_row($result);
     header("Location: http://localhost:8080/CS2102-Project/app/main.php?username=".$row[0]."&is_admin=".$row[1]);
   }
+
 } else {
   header("Location: http://localhost:8080/CS2102-Project/app/index.php");
 }
