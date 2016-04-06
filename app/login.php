f4028d07b5caefa598d124119002b8912edcab6b
Jack Song <jackthesong@gmail.com>
2016-04-06 19:30:28 +0800

    fix registration

--- app/login.php
+++ app/login.php
@@ -5,13 +5,13 @@ if (isset($_GET['username'])) {
 
   //registering
   if($_GET['submitBtn'] == "Register") {
-    $countquery = "SELECT COUNT(*) as total FROM carpool.carpool.users";
-    $countresult = pg_query($query) or die('Query failed: ' . pg_last_error());
-    $rowcount = pg_fetch_row($countresult);
+    $countquery = "SELECT * FROM carpool.carpool.users";
+    $countresult = pg_query($countquery) or die('Query failed: ' . pg_last_error());
+    $rowcount = pg_num_rows($countresult);
 
     $query = "INSERT INTO carpool.carpool.users VALUES ('".$_GET['username']."', '".$_GET['email']."', '".$_GET['firstname']."', '".$_GET['lastname']."', ".($rowcount[0] + 1).", false)";
     $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-    if ($pg_num_rows($result) == 0) {
+    if (pg_num_rows($result) == 0) {
       header("Location: http://localhost:8080/CS2102-Project/app/index.php?error=1");
     }
   }
@@ -24,7 +24,7 @@ if (isset($_GET['username'])) {
   } else {
     $row = pg_fetch_row($result);
     header("Location: http://localhost:8080/CS2102-Project/app/main.php?username=".$row[0]."&is_admin=".$row[1]);
-  }
+  } 
 
 } else {
   header("Location: http://localhost:8080/CS2102-Project/app/index.php");
